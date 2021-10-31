2089d17c152b1d974eca5869d71aef36c379af21
LIZIHRONG <asbkk3697@gmail.com>
2021-10-31 12:50:02 +0800

    nav fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const nav = () => {
 	return (
 		<nav>
 			<ul>
 				<li>
-					<Link to="/">Home</Link>
+					<Link to="/photoWebsiteReactPractice">Home</Link>
 				</li>
 				<li>
-					<Link to="/about">About</Link>
+					<Link to="/photoWebsiteReactPractice/about">About</Link>
 				</li>
 			</ul>
 		</nav>
-	);
-};
+	)
+}
 
-export default nav;
+export default nav
