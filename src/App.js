cd05557644216567157f6f27cc3c7d801970483b
LIZIHRONG <asbkk3697@gmail.com>
2021-10-31 12:43:00 +0800

    1031

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import Footer from './components/Footer';
-import Nav from './components/Nav';
-import Homepage from './pages/Homepage';
-import About from './pages/About';
-import { Switch, Route } from 'react-router-dom';
-import './styles/style.css';
+import React from 'react'
+import Footer from './components/Footer'
+import Nav from './components/Nav'
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import { Switch, Route } from 'react-router-dom'
+import './styles/style.css'
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
 			<Switch>
-				<Route path="/" exact>
+				<Route path="/photoWebsite-ReactPractice" exact>
 					<Homepage />
 				</Route>
-				<Route path="/about" exact>
+				<Route path="/photoWebsite-ReactPractice/about" exact>
 					<About />
 				</Route>
 			</Switch>
 			<Footer />
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
